171e6ab6e9f5e22c776b93278680a8ebe045cf48
Tahmina Akter Anondi <68536783+TahminaAnondi@users.noreply.github.com>
2024-03-12 15:02:00 -0500

    Add files via upload

---
+++ Products.java
@@ -0,0 +1,14 @@
+package constructor;
+
+public class Products {
+ public String name ="IPhone";
+ public double price = 300.00; 
+ 
+ public Products(String n) {
+	 name = n;
+ }
+ public Products(String n, double p) {
+	 name = n;
+	 price = p;
+ }
+}
